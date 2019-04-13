d4805c9fe8a3925ed2887381602f2e102ad07763
zanenkn <zane.neikena@gmail.com>
2019-04-14 01:03:44 +0200

    Modifies the result message (replaces eql with include in specs, for now)

--- spec/fizz-buzz.spec.js
+++ spec/fizz-buzz.spec.js
@@ -4,20 +4,22 @@ describe('Fizz Buzz', () => {
     let fizzBuzz = new FizzBuzz
 
     it("returns a number if no game rules are met", () => {
-        expect(fizzBuzz.check(1)).to.eql(1)
+        expect(fizzBuzz.check(1)).to.include(1)
     })
 
     it("returns Buzz if number is divisible by 5", () => {
-        expect(fizzBuzz.check(5)).to.eql("Buzz")
+        expect(fizzBuzz.check(5)).to.include("Buzz!")
     })
 
     it ("returns Fizz if number is divisible by 3", () => {
-        expect(fizzBuzz.check(3)).to.eql("Fizz")
+        expect(String(fizzBuzz.check(3))).to.include("Fizz!")
     })
 
     it ("returns FizzBuzz if number is divisible by 15", () => {
-        expect(fizzBuzz.check(15)).to.eql("FizzBuzz")
+        expect(fizzBuzz.check(15)).to.include("FizzBuzz!")
     })
+
+
 
 })
 
--- src/js/fizz-buzz.js
+++ src/js/fizz-buzz.js
@@ -1,13 +1,13 @@
 function FizzBuzz()  {
     this.check = (number) => {
         if (number % 15 === 0){
-            return "FizzBuzz"
+            return `You entered ${number}. <br> <h1>FizzBuzz!</h1>`;
         } else if (number % 5 === 0){
-            return "Buzz";
+            return `You entered ${number}. <br> <h1>Buzz!</h1>`;
         } else if (number % 3 === 0){
-            return "Fizz";
+            return `You entered ${number}. <br> <h1>Fizz!</h1>`;
         } else {
-            return number
+            return `You entered ${number}.`
         }
     }
 }
