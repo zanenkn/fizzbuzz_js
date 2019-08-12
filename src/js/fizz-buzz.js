390bd341238141d9f835d7e7874b118a21333b73
zanenkn <zane.neikena@gmail.com>
2019-08-12 13:38:37 +0200

    Adjustments to accomodate Tailwind update to 1.0

--- src/js/fizz-buzz.js
+++ src/js/fizz-buzz.js
@@ -1,13 +1,13 @@
 function FizzBuzz()  {
     this.check = (number) => {
         if (number === "0" || number == "") {
-            return `No number no party, friend! <br> <h1>No Buzz!</h1>`
+            return `No number no party, friend! <br> <h1 class="text-xl font-semibold">No Buzz!</h1>`
         } else if (number % 15 === 0){
-            return `You entered ${number}. <br> <h1>FizzBuzz!</h1>`;
+            return `You entered ${number}. <br> <h1 class="text-xl font-semibold">FizzBuzz!</h1>`;
         } else if (number % 5 === 0){
-            return `You entered ${number}. <br> <h1>Buzz!</h1>`;
+            return `You entered ${number}. <br> <h1 class="text-xl font-semibold">Buzz!</h1>`;
         } else if (number % 3 === 0){
-            return `You entered ${number}. <br> <h1>Fizz!</h1>`;
+            return `You entered ${number}. <br> <h1 class="text-xl font-semibold">Fizz!</h1>`;
         } else {
             return `You entered ${number}.`
         }
